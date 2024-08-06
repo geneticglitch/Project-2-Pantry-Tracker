6768dcfaae439e975c25cc2ea3fa86a4b069eb3f
Aryan Singh <s.aryan0327@gmail.com>
2024-08-06 10:53:48 -0500

    minor development error fixes

--- client/src/app/auth/signin/page.tsx
+++ client/src/app/auth/signin/page.tsx
@@ -20,13 +20,11 @@ export default function SignIn() {
             redirect: true,
             display_name: displayName,
             password: password,
-            callbackUrl:"/",
+            callbackUrl:"/dashboard",
         });
 
         if (result?.error) {
             alert(result.error);
-        } else {
-            window.location.href = "/";
         }
     }
   return (
@@ -51,7 +49,7 @@ export default function SignIn() {
                         </div>
                         <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                         <p className="text-sm font-light dark:text-gray-400">
-                            Don't have an account yet? <a href="/auth/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                            {"Don't have an account yet?"} <a href="/auth/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                         </p>
                     </form>
                 </div>
